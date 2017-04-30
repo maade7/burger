4e04bdde3e94e38e95e3c55a62ec019bf1ab9df3
maade7 <martin.ade@gatech.edu>
2017-04-30 15:04:40 -0400

    heroku JawsDB

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var methodOverride = require("method-override");
 
 
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
